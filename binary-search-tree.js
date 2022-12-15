9a106d28ffe297aabe73f8015fbd42671da97f7a
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2022-12-14 17:36:43 -0800

    Finish insert recursion, find, find recursion, and dfspreorder methods

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -11,15 +11,35 @@ class Node {
   * Returns the node, if found; else undefined. Uses recursion. */
 
   findRecursively(val) {
-
-
+    if(val === this.val) {
+      return this;
+    }
+    if(val < this.val) {
+      if(!this.left) return undefined;
+      return this.left.findRecursively(val);
+    } else {
+      if(!this.right) return undefined;
+      return this.right.findRecursively(val);
+    }
   }
 
   /** insertRecursively(val): Starting at the invoking node, insert a new node
    * into the BST with value val. Returns the inserted node. Uses recursion. */
 
   insertRecursively(val) {
-
+    if(val < this.val) {
+      if(!this.left) {
+        this.left = new Node(val);
+        return this;
+      }
+      return this.left.insertRecursively(val);
+    } else {
+      if(!this.right) {
+        this.right = new Node(val);
+        return this;
+      }
+      return this.right.insertRecursively(val);
+    }
   }
 
   /** dfsPreOrder(): Traverse from the invoking node using pre-order DFS.
@@ -27,6 +47,18 @@ class Node {
 
   dfsPreOrder() {
 
+    let nodesArray = [];
+
+    if (this === null) return nodesArray;
+    nodesArray.push(this.val);
+    if(this.left) {
+      nodesArray = [...nodesArray, ...this.left.dfsPreOrder()];
+    }
+    if(this.right) {
+      nodesArray = [...nodesArray, ...this.right.dfsPreOrder()];
+    }
+
+    return nodesArray;
   }
 
   /** dfsInOrder(): Traverse from the invoking node using in-order DFS.
@@ -86,28 +118,50 @@ class BinarySearchTree {
    * Returns the tree instance. Uses recursion. */
 
   insertRecursively(val) {
+    //edge case
+    if (this.root === null) {
+      this.root = new Node(val);
+      return this;
+    }
 
+    this.root.insertRecursively(val);
+    return this;
   }
 
   /** find(val): Search the BST for a node with value val.
    * Returns the node, if found; else undefined. Uses iteration. */
 
   find(val) {
+    let current = this.root;
 
+    while (current) {
+      if (current.val === val)
+        return current;
+
+      current = (val < current.val)
+        ? current.left
+        : current.right;
+    }
+
+    return undefined;
   }
 
   /** findRecursively(val): Search the BST for a node with value val.
    * Returns the node, if found; else undefined. Uses recursion. */
 
   findRecursively(val) {
+    if (this.root === null) return undefined;
 
+    return this.root.findRecursively(val);
   }
 
   /** dfsPreOrder(): Traverse the BST using pre-order DFS.
    * Returns an array of visited nodes. */
 
   dfsPreOrder() {
-
+    if (this.root === null) return [];
+    
+    return this.root.dfsPreOrder();
   }
 
   /** dfsInOrder(): Traverse the BST using in-order DFS.
